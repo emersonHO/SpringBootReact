6ab35765f4a42a66dbb7a9541eedad10c2c85476
JesusCondor16 <jesus.condor3@unmsm.edu.pe>
2024-07-09 20:52:54 -0500

    Actualizando back

--- modulo-cursos-backend/src/main/java/com/modulo/cursos/controller/CursosController.java
+++ modulo-cursos-backend/src/main/java/com/modulo/cursos/controller/CursosController.java
@@ -1,13 +1,11 @@
 package com.modulo.cursos.controller;
 
-import com.modulo.cursos.exception.ResourceNotFoundException;
 import com.modulo.cursos.model.Curso;
-import com.modulo.cursos.repository.ICursoRepository;
+import com.modulo.cursos.service.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,61 +15,33 @@ import java.util.Map;
 public class CursosController {
 
     @Autowired
-    private ICursoRepository cursoRepository;
+    private CursoService cursoService;
 
     @GetMapping("/cursos")
-    public List<Curso> listarCursos(){
-        return cursoRepository.findAll();
+    public List<Curso> listarCursos() {
+        return cursoService.listarCursos();
     }
 
     @PostMapping("/cursos")
-    public Curso guardarCuso(@RequestBody Curso curso){
-        return cursoRepository.save(curso);
+    public Curso guardarCurso(@RequestBody Curso curso) {
+        return cursoService.guardarCurso(curso);
     }
 
     @GetMapping("/cursos/{id}")
-    public ResponseEntity<Curso> listarCursoPorId(@PathVariable Long id){
-        Curso curso = cursoRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: "+id));
+    public ResponseEntity<Curso> listarCursoPorId(@PathVariable Long id) {
+        Curso curso = cursoService.listarCursoPorId(id);
         return ResponseEntity.ok(curso);
     }
 
     @PutMapping("/cursos/{id}")
-    public ResponseEntity<Curso> actualizarCurso(@PathVariable Long id, @RequestBody Curso cursoRequest){
-        Curso curso = cursoRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: "+id));
-
-        curso.setCiclo(cursoRequest.getCiclo());
-        curso.setCodigo(cursoRequest.getCodigo());
-        curso.setEstado(cursoRequest.getEstado());
-        curso.setNombre(cursoRequest.getNombre());
-        curso.setModalidad(cursoRequest.getModalidad());
-        curso.setDepartamento_id(cursoRequest.getDepartamento_id());
-        curso.setInstitucion_id(cursoRequest.getInstitucion_id());
-        curso.setNum_creditos(cursoRequest.getNum_creditos());
-        curso.setTipo(cursoRequest.getTipo());
-        curso.setSumilla(cursoRequest.getSumilla());
-        curso.setNum_horas_campo(cursoRequest.getNum_horas_campo());
-        curso.setNum_horas_laboratorio(cursoRequest.getNum_horas_laboratorio());
-        curso.setNum_horas_practica(cursoRequest.getNum_horas_practica());
-        curso.setNum_horas_teoria(cursoRequest.getNum_horas_teoria());
-        curso.setPeriodo_academico_id(cursoRequest.getPeriodo_academico_id());
-        curso.setPlan_estudios_id(cursoRequest.getPlan_estudios_id());
-        curso.setDia(cursoRequest.getDia());
-        curso.setHora_inicio(cursoRequest.getHora_inicio());
-        curso.setHora_fin(cursoRequest.getHora_fin());
-        Curso cursoActualizado = cursoRepository.save(curso);
+    public ResponseEntity<Curso> actualizarCurso(@PathVariable Long id, @RequestBody Curso cursoRequest) {
+        Curso cursoActualizado = cursoService.actualizarCurso(id, cursoRequest);
         return ResponseEntity.ok(cursoActualizado);
     }
 
     @DeleteMapping("/cursos/{id}")
-    public ResponseEntity<Map<String, Boolean>> eliminarCurso (@PathVariable Long id){
-        Curso curso = cursoRepository.findById(id).
-                orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: "+id));
-
-        cursoRepository.delete(curso);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
+    public ResponseEntity<Map<String, Boolean>> eliminarCurso(@PathVariable Long id) {
+        Map<String, Boolean> response = cursoService.eliminarCurso(id);
         return ResponseEntity.ok(response);
     }
 }
---
+++ modulo-cursos-backend/src/main/java/com/modulo/cursos/service/CursoService.java
@@ -0,0 +1,68 @@
+package com.modulo.cursos.service;
+
+import com.modulo.cursos.exception.ResourceNotFoundException;
+import com.modulo.cursos.model.Curso;
+import com.modulo.cursos.repository.ICursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+@Service
+public class CursoService {
+
+    @Autowired
+    private ICursoRepository cursoRepository;
+
+    public List<Curso> listarCursos() {
+        return cursoRepository.findAll();
+    }
+
+    public Curso guardarCurso(Curso curso) {
+        return cursoRepository.save(curso);
+    }
+
+    public Curso listarCursoPorId(Long id) {
+        return cursoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: " + id));
+    }
+
+    public Curso actualizarCurso(Long id, Curso cursoRequest) {
+        Curso curso = cursoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: " + id));
+
+        curso.setCiclo(cursoRequest.getCiclo());
+        curso.setCodigo(cursoRequest.getCodigo());
+        curso.setEstado(cursoRequest.getEstado());
+        curso.setNombre(cursoRequest.getNombre());
+        curso.setModalidad(cursoRequest.getModalidad());
+        curso.setDepartamento_id(cursoRequest.getDepartamento_id());
+        curso.setInstitucion_id(cursoRequest.getInstitucion_id());
+        curso.setNum_creditos(cursoRequest.getNum_creditos());
+        curso.setTipo(cursoRequest.getTipo());
+        curso.setSumilla(cursoRequest.getSumilla());
+        curso.setNum_horas_campo(cursoRequest.getNum_horas_campo());
+        curso.setNum_horas_laboratorio(cursoRequest.getNum_horas_laboratorio());
+        curso.setNum_horas_practica(cursoRequest.getNum_horas_practica());
+        curso.setNum_horas_teoria(cursoRequest.getNum_horas_teoria());
+        curso.setPeriodo_academico_id(cursoRequest.getPeriodo_academico_id());
+        curso.setPlan_estudios_id(cursoRequest.getPlan_estudios_id());
+        curso.setDia(cursoRequest.getDia());
+        curso.setHora_inicio(cursoRequest.getHora_inicio());
+        curso.setHora_fin(cursoRequest.getHora_fin());
+        return cursoRepository.save(curso);
+    }
+
+    public Map<String, Boolean> eliminarCurso(Long id) {
+        Curso curso = cursoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: " + id));
+
+        cursoRepository.delete(curso);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+}
+
