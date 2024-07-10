213fb249bd354cb59676db263ecf1a3b8615ea86
JesusCondor16 <jesus.condor3@unmsm.edu.pe>
2024-07-09 21:44:22 -0500

    Actualizando backend

--- modulo-cursos-backend/src/main/java/com/modulo/cursos/controller/AlumnoCursoController.java
+++ modulo-cursos-backend/src/main/java/com/modulo/cursos/controller/AlumnoCursoController.java
@@ -1,12 +1,7 @@
 package com.modulo.cursos.controller;
 
-import com.modulo.cursos.exception.ResourceNotFoundException;
-import com.modulo.cursos.model.Alumno;
 import com.modulo.cursos.model.AlumnoCurso;
-import com.modulo.cursos.model.Curso;
-import com.modulo.cursos.repository.IAlumnoCursoRepository;
-import com.modulo.cursos.repository.IAlumnoRepository;
-import com.modulo.cursos.repository.ICursoRepository;
+import com.modulo.cursos.service.AlumnoCursoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,40 +14,20 @@ import java.util.List;
 public class AlumnoCursoController {
 
     @Autowired
-    private IAlumnoCursoRepository alumnoCursoRepository;
-
-    @Autowired
-    private IAlumnoRepository alumnoRepository;
-
-    @Autowired
-    private ICursoRepository cursoRepository;
+    private AlumnoCursoService alumnoCursoService;
 
     @PostMapping("/cursos/{cursoId}/alumnos")
     public ResponseEntity<AlumnoCurso> asociarAlumnoConCurso(
             @PathVariable Long cursoId,
             @RequestParam Long alumnoId,
             @RequestParam String estado) {
-        Curso curso = cursoRepository.findById(cursoId)
-                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: " + cursoId));
-
-        Alumno alumno = alumnoRepository.findById(alumnoId)
-                .orElseThrow(() -> new ResourceNotFoundException("El alumno con ese ID no existe: " + alumnoId));
-
-        AlumnoCurso alumnoCurso = new AlumnoCurso();
-        alumnoCurso.setCurso(curso);
-        alumnoCurso.setAlumno(alumno);
-        alumnoCurso.setEstado(estado);
-
-        AlumnoCurso nuevoAlumnoCurso = alumnoCursoRepository.save(alumnoCurso);
+        AlumnoCurso nuevoAlumnoCurso = alumnoCursoService.asociarAlumnoConCurso(cursoId, alumnoId, estado);
         return ResponseEntity.ok(nuevoAlumnoCurso);
     }
 
     @GetMapping("/cursos/{cursoId}/alumnos")
     public ResponseEntity<List<AlumnoCurso>> listarAlumnosPorCurso(@PathVariable Long cursoId) {
-        Curso curso = cursoRepository.findById(cursoId)
-                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: " + cursoId));
-
-        List<AlumnoCurso> alumnosDelCurso = alumnoCursoRepository.findByCurso(curso);
+        List<AlumnoCurso> alumnosDelCurso = alumnoCursoService.listarAlumnosPorCurso(cursoId);
         return ResponseEntity.ok(alumnosDelCurso);
     }
 }
---
+++ modulo-cursos-backend/src/main/java/com/modulo/cursos/service/AlumnoCursoService.java
@@ -0,0 +1,49 @@
+package com.modulo.cursos.service;
+
+import com.modulo.cursos.exception.ResourceNotFoundException;
+import com.modulo.cursos.model.Alumno;
+import com.modulo.cursos.model.AlumnoCurso;
+import com.modulo.cursos.model.Curso;
+import com.modulo.cursos.repository.IAlumnoCursoRepository;
+import com.modulo.cursos.repository.IAlumnoRepository;
+import com.modulo.cursos.repository.ICursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlumnoCursoService {
+
+    @Autowired
+    private IAlumnoCursoRepository alumnoCursoRepository;
+
+    @Autowired
+    private IAlumnoRepository alumnoRepository;
+
+    @Autowired
+    private ICursoRepository cursoRepository;
+
+    public AlumnoCurso asociarAlumnoConCurso(Long cursoId, Long alumnoId, String estado) {
+        Curso curso = cursoRepository.findById(cursoId)
+                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: " + cursoId));
+
+        Alumno alumno = alumnoRepository.findById(alumnoId)
+                .orElseThrow(() -> new ResourceNotFoundException("El alumno con ese ID no existe: " + alumnoId));
+
+        AlumnoCurso alumnoCurso = new AlumnoCurso();
+        alumnoCurso.setCurso(curso);
+        alumnoCurso.setAlumno(alumno);
+        alumnoCurso.setEstado(estado);
+
+        return alumnoCursoRepository.save(alumnoCurso);
+    }
+
+    public List<AlumnoCurso> listarAlumnosPorCurso(Long cursoId) {
+        Curso curso = cursoRepository.findById(cursoId)
+                .orElseThrow(() -> new ResourceNotFoundException("El curso con ese ID no existe: " + cursoId));
+
+        return alumnoCursoRepository.findByCurso(curso);
+    }
+}
+
